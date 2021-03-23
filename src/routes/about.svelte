20641596dbcf4eec1a107e84e7d56ecc7e31d59c
swyx <swyx@dontemail.me>
2021-03-24 03:23:52 +0800

    init

---
+++ src/routes/about.svelte
@@ -0,0 +1,4 @@
+
+<main>
+	<h1>About!</h1>
+</main>
---
+++ src/routes/settings/$layout.svelte
@@ -0,0 +1,9 @@
+<!-- src/routes/settings/$layout.svelte -->
+<h1>Settings</h1>
+
+<div class="submenu">
+	<a href="/settings/profile">Profile</a>
+	<a href="/settings/notifications">Notifications</a>
+</div>
+
+<slot></slot>
